f01603db3662571ed21b40d240b8780209b8bc76
britg <brit@britg.com>
2011-06-18 16:10:52 -0500

    show menu on custom view click. dynamically resize the view depending on stats size

--- New Relic Menus/MainMenuController.m
+++ New Relic Menus/MainMenuController.m
@@ -11,6 +11,16 @@
 
 @implementation MainMenuController
 
+@synthesize menuView;
+@synthesize throughputLabel;
+@synthesize throughputUnits;
+@synthesize responseTimeLabel;
+@synthesize responseTimeUnits;
+@synthesize errorRateLabel;
+@synthesize errorRateUnits;
+@synthesize apdexLabel;
+@synthesize apdexUnits;
+
 - (id)init
 {
     self = [super init];
@@ -18,6 +28,8 @@
         mainStatusItem = [[[NSStatusBar systemStatusBar] 
                            statusItemWithLength:NSVariableStatusItemLength] retain];
         [self listenForNotifications];
+        [NSBundle loadNibNamed:@"MenuView" owner:self];
+        DebugLog(@"menu view is %@", self.menuView);
     }
     
     return self;
@@ -63,6 +75,11 @@
                                              selector:@selector(getPrimaryMetrics) 
                                                  name:APPLICATION_OBTAINED_NOTIFICATION
                                                object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self 
+                                             selector:@selector(showMainMenu) 
+                                                 name:STATS_VIEW_CLICKED_NOTIFICATION
+                                               object:nil];
 }
 
 #pragma mark - States
@@ -77,13 +94,79 @@
 }
 
 - (void)statsUpdated {
-    DebugLog(@"Status updated!");
+    DebugLog(@"Stats updated!");
+    
+    /*
     [mainStatusItem setTitle:[NSString stringWithFormat:@"%.00frpm %.0fms %.02ferr %.01fapdex", 
                               [[[APIHandler sharedInstance] throughput] floatValue],
                               [[[APIHandler sharedInstance] responseTime] floatValue],
                               [[[APIHandler sharedInstance] errorPercent] floatValue],
                               [[[APIHandler sharedInstance] apdex] floatValue]
                               ]];
+     */
+    NSRect s;
+    NSRect u;
+    int offset;
+    
+    [self.throughputLabel setStringValue:[[[APIHandler sharedInstance] throughput] stringValue]];
+    [self.throughputLabel sizeToFit];
+    
+    s = self.throughputLabel.frame;
+    offset = s.size.width + s.origin.x - 4;
+    u = self.throughputUnits.frame;
+    
+    [self.throughputUnits setFrame:NSMakeRect(offset, u.origin.y, u.size.width, u.size.height)];
+    
+    u = self.throughputUnits.frame;
+    offset = u.size.width + u.origin.x;
+    
+    
+    [self.responseTimeLabel setStringValue:[NSString stringWithFormat:@"%.0f", [[[APIHandler sharedInstance] responseTime] floatValue]]];
+    [self.responseTimeLabel sizeToFit];
+    s = self.responseTimeLabel.frame;
+    [self.responseTimeLabel setFrame:NSMakeRect(offset, s.origin.y, s.size.width, s.size.height)];
+    
+    s = self.responseTimeLabel.frame;
+    offset = s.size.width + s.origin.x - 4;
+    u = self.responseTimeUnits.frame;
+    
+    [self.responseTimeUnits setFrame:NSMakeRect(offset, u.origin.y, u.size.width, u.size.height)];
+    
+    u = self.responseTimeUnits.frame;
+    offset = u.size.width + u.origin.x;
+    
+    [self.errorRateLabel setStringValue:[NSString stringWithFormat:@"%.02f", [[[APIHandler sharedInstance] errorPercent] floatValue]]];
+    [self.errorRateLabel sizeToFit];
+    s = self.errorRateLabel.frame;
+    [self.errorRateLabel setFrame:NSMakeRect(offset, s.origin.y, s.size.width, s.size.height)];
+    
+    s = self.errorRateLabel.frame;
+    offset = s.size.width + s.origin.x - 4;
+    u = self.errorRateUnits.frame;
+    
+    [self.errorRateUnits setFrame:NSMakeRect(offset, u.origin.y, u.size.width, u.size.height)];
+    
+    u = self.errorRateUnits.frame;
+    offset = u.size.width + u.origin.x;
+    
+    [self.apdexLabel setStringValue:[NSString stringWithFormat:@"%.02f", [[[APIHandler sharedInstance] apdex] floatValue]]];
+    [self.apdexLabel sizeToFit];
+    s = self.apdexLabel.frame;
+    [self.apdexLabel setFrame:NSMakeRect(offset, s.origin.y, s.size.width, s.size.height)];
+    
+    s = self.apdexLabel.frame;
+    offset = s.size.width + s.origin.x - 4;
+    u = self.apdexUnits.frame;
+    
+    [self.apdexUnits setFrame:NSMakeRect(offset, u.origin.y, u.size.width, u.size.height)];
+    
+    u = self.apdexUnits.frame;
+    offset = u.size.width + u.origin.x;
+    s = self.menuView.frame;
+    
+    [self.menuView setFrame:NSMakeRect(s.origin.x, s.origin.y, offset, s.size.height)];
+    
+    [mainStatusItem setView:self.menuView];
 }
 
 #pragma mark - Actions
@@ -118,6 +201,10 @@
 - (void)getPrimaryMetrics {
     DebugLog(@"Fetching primary metrics");
     [[APIHandler sharedInstance] getPrimaryMetrics];
+}
+
+- (void)showMainMenu {
+    [mainStatusItem popUpStatusItemMenu:menu];
 }
 
 @end
