f26a726b010bc7111f896835aebf21f7589e1b93
britg <brit@britg.com>
2011-07-04 08:47:10 -0500

    Selectable current application

--- New Relic Menus/MainMenuController.m
+++ New Relic Menus/MainMenuController.m
@@ -8,6 +8,7 @@
 
 #import "MainMenuController.h"
 #import "APIHandler.h"
+#import "NRMApplication.h"
 
 @implementation MainMenuController
 
@@ -49,17 +50,28 @@
     NSZone *menuZone = [NSMenu menuZone];
 	menu = [[NSMenu allocWithZone:menuZone] init];
     
+    /* Create the Application switcher */
+    NSMenuItem *appSwitcherItem = [[NSMenuItem alloc] initWithTitle:@"Select Application" action:nil keyEquivalent:@"a"];
+    appSubMenu = [[NSMenu allocWithZone:menuZone] init];
+    
+    [appSwitcherItem setSubmenu:appSubMenu];
+    [appSwitcherItem setToolTip:@"Switch applications"];
+    [appSwitcherItem setTarget:self];
+    [menu addItem:appSwitcherItem];
+    
+    [menu addItem:[NSMenuItem separatorItem]];
+    
     NSMenuItem *menuItem;
     
-    menuItem = [menu addItemWithTitle:@"New Relic Dashboard" action:@selector(openNewRelicDashboard) keyEquivalent:@"O"];
+    menuItem = [menu addItemWithTitle:@"New Relic Dashboard" action:@selector(openNewRelicDashboard) keyEquivalent:@"o"];
     [menuItem setToolTip:@"Open Your New Relic Dashboard"];
     [menuItem setTarget:self];
     
-    menuItem = [menu addItemWithTitle:@"Preferences" action:@selector(notifyPreferencesAction) keyEquivalent:@"P"];
+    menuItem = [menu addItemWithTitle:@"Preferences" action:@selector(notifyPreferencesAction) keyEquivalent:@"p"];
     [menuItem setToolTip:@"Change your API Key"];
     [menuItem setTarget:self];
     
-    menuItem = [menu addItemWithTitle:@"Quit" action:@selector(notifyQuit) keyEquivalent:@"Q"];
+    menuItem = [menu addItemWithTitle:@"Quit" action:@selector(notifyQuit) keyEquivalent:@"q"];
     [menuItem setToolTip:@"Quit New Relic Menus"];
     [menuItem setTarget:self];
     
@@ -71,8 +83,13 @@
 
 - (void)listenForNotifications {
     [[NSNotificationCenter defaultCenter] addObserver:self 
-                                             selector:@selector(statsUpdated) 
+                                             selector:@selector(statsUpdated)
                                                  name:METRICS_OBTAINED_NOTIFICATION
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self 
+                                             selector:@selector(applicationsUpdated) 
+                                                 name:APPLICATION_OBTAINED_NOTIFICATION
                                                object:nil];
     
     [[NSNotificationCenter defaultCenter] addObserver:self 
@@ -93,26 +110,27 @@
 }
 
 - (void)setStateLoading {
-    //[mainStatusItem setLength:100];
     [mainStatusItem setTitle:@"Loading..."];
 }
 
 - (void)statsUpdated {
     DebugLog(@"Stats updated!");
     
-    /*
-    [mainStatusItem setTitle:[NSString stringWithFormat:@"%.00frpm %.0fms %.02ferr %.01fapdex", 
-                              [[[APIHandler sharedInstance] throughput] floatValue],
-                              [[[APIHandler sharedInstance] responseTime] floatValue],
-                              [[[APIHandler sharedInstance] errorPercent] floatValue],
-                              [[[APIHandler sharedInstance] apdex] floatValue]
-                              ]];
-     */
+    [self.throughputLabel setStringValue:[[[APIHandler sharedInstance] throughput] stringValue]];
+    [self.responseTimeLabel setStringValue:[NSString stringWithFormat:@"%.0f", [[[APIHandler sharedInstance] responseTime] floatValue]]];
+    [self.errorRateLabel setStringValue:[NSString stringWithFormat:@"%.02f", [[[APIHandler sharedInstance] errorPercent] floatValue]]];
+    [self.apdexLabel setStringValue:[NSString stringWithFormat:@"%.02f", [[[APIHandler sharedInstance] apdex] floatValue]]];
+    
+    [self adjustLayout];
+    
+    [mainStatusItem setView:self.menuView];
+}
+
+- (void)adjustLayout {
     NSRect s;
     NSRect u;
     int offset;
     
-    [self.throughputLabel setStringValue:[[[APIHandler sharedInstance] throughput] stringValue]];
     [self.throughputLabel sizeToFit];
     
     s = self.throughputLabel.frame;
@@ -125,7 +143,7 @@
     offset = u.size.width + u.origin.x;
     
     
-    [self.responseTimeLabel setStringValue:[NSString stringWithFormat:@"%.0f", [[[APIHandler sharedInstance] responseTime] floatValue]]];
+    
     [self.responseTimeLabel sizeToFit];
     s = self.responseTimeLabel.frame;
     [self.responseTimeLabel setFrame:NSMakeRect(offset, s.origin.y, s.size.width, s.size.height)];
@@ -139,7 +157,7 @@
     u = self.responseTimeUnits.frame;
     offset = u.size.width + u.origin.x;
     
-    [self.errorRateLabel setStringValue:[NSString stringWithFormat:@"%.02f", [[[APIHandler sharedInstance] errorPercent] floatValue]]];
+    
     [self.errorRateLabel sizeToFit];
     s = self.errorRateLabel.frame;
     [self.errorRateLabel setFrame:NSMakeRect(offset, s.origin.y, s.size.width, s.size.height)];
@@ -153,7 +171,7 @@
     u = self.errorRateUnits.frame;
     offset = u.size.width + u.origin.x;
     
-    [self.apdexLabel setStringValue:[NSString stringWithFormat:@"%.02f", [[[APIHandler sharedInstance] apdex] floatValue]]];
+    
     [self.apdexLabel sizeToFit];
     s = self.apdexLabel.frame;
     [self.apdexLabel setFrame:NSMakeRect(offset, s.origin.y, s.size.width, s.size.height)];
@@ -169,8 +187,21 @@
     s = self.menuView.frame;
     
     [self.menuView setFrame:NSMakeRect(s.origin.x, s.origin.y, offset, s.size.height)];
-    
-    [mainStatusItem setView:self.menuView];
+}
+
+- (void)applicationsUpdated {
+    NSMenuItem *menuItem;
+    NRMApplication *app;
+    NSArray *apps = [[APIHandler sharedInstance] applications];
+    for (int i = 0; i < apps.count; i++) {
+        app = [apps objectAtIndex:i];
+        menuItem = [appSubMenu addItemWithTitle:app.name 
+                                         action:@selector(setCurrentApplication:) 
+                                  keyEquivalent:[NSString stringWithFormat:@"%i", i]];
+        [menuItem setToolTip:[NSString stringWithFormat:@"Set application to %@", app.name]];
+        [menuItem setTarget:self];
+        [menuItem setRepresentedObject:app];
+    }
 }
 
 #pragma mark - Actions
@@ -213,6 +244,11 @@
 
 - (void)showMainMenu {
     [mainStatusItem popUpStatusItemMenu:menu];
+}
+
+- (void)setCurrentApplication:(NSMenuItem *)selectedMenuItem {
+    NRMApplication *app = [selectedMenuItem representedObject];
+    [[APIHandler sharedInstance] setCurrentApplication:app.appId];
 }
 
 @end
